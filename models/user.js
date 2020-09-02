623720d3587fce43f9fd6846dc46fa3115d258c6
Farhan Farooq <farhan.farooq@live.com>
2020-09-02 23:09:36 +0500

    user register / login, auth and admin middlewares

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,      
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    about: {
+      type: String,
+      trim: true,
+    },
+    role: {
+      type: Number,
+      default: 0, // user = 0, admin = 1
+    },
+    history: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 require("dotenv").config();
 
 // Connect to DB
@@ -13,6 +14,13 @@ mongoose
   })
   .then(() => console.log("mongoDB is connected"))
   .catch((err) => console.log(err));
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
