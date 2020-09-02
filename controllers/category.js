bb7a0f7af5c3de61a25ec0ccfd25bb81944691ac
Farhan Farooq <farhan.farooq@live.com>
2020-09-03 02:47:28 +0500

    get categories, update and delete category

--- controllers/category.js
+++ controllers/category.js
@@ -9,3 +9,55 @@ exports.create = async (req, res) => {
     console.log(err);
   }
 };
+
+exports.getCategories = async (req, res) => {
+  try {
+    const category = await Category.find();
+    if (!category) {
+      return res.status(400).json({ error: "Categories not found" });
+    }
+    res.json(category);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.getCategory = async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.categoryId);
+    if (!category) {
+      return res.status(400).json({ error: "Category not found" });
+    }
+    res.json(category);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.updateCategory = async (req, res) => {
+  try {
+    let category = await Category.findByIdAndUpdate(
+      req.params.categoryId,
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.json(category);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.deleteCategory = async (req, res) => {
+  try {
+    let category = await Category.findById(req.params.categoryId);
+    if (!category) {
+      return res.status(400).json({ error: "Category not found" });
+    }
+    await category.remove();
+    res.json({ message: "Category successfully deleted" });
+  } catch (err) {
+    console.log(err);
+  }
+};
--- routes/category.js
+++ routes/category.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { create } = require("../controllers/category");
+const {
+  create,
+  getCategory,
+  updateCategory,
+  deleteCategory,
+  getCategories
+} = require("../controllers/category");
 const { requireLogin, isAdmin } = require("../middleware/auth");
 
 router.post("/create", requireLogin, isAdmin, create);
+router.get("/:categoryId", getCategory);
+router.put("/:categoryId/:userId", requireLogin, isAdmin, updateCategory);
+router.delete("/:categoryId/:userId", requireLogin, isAdmin, deleteCategory);
+router.get("/", getCategories);
 
 module.exports = router;
